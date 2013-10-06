789bc21db329262a0d8be94b7cf5667453784218
Yosuke Kabuto <bluewhale1982@gmail.com>
2013-10-07 01:47:59 +0900

    refactored

--- opai.c
+++ opai.c
@@ -7,19 +7,20 @@ void main()
   struct stat theStat;
   FILE *fp;
 
-  fp = fopen("opai.txt", "w");
-
-  if (fp)
+  if (fp = fopen("opai.txt", "w"))
   {
-    int fsize = 0;
-    int mb = 1024 * 1024; // 1MB
+    int fsize;
+    int count;
+    const int ONE_MB = 1024 * 1024; // 1MB
 
-    for (fsize; fsize < mb; fsize = theStat.st_size)
+    for (fsize = 0, count = 0; fsize < ONE_MB; fsize = theStat.st_size)
     {
       stat("opai.txt", &theStat);
       fprintf(fp, "おっぱい"); // おっぱい is paiotsu
+      count++;
     }
 
     fclose(fp);
+    printf("%d-おっぱい are about 1MB.\n", count);
   }
 }
