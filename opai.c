8228e00464a15c9f7c8347453fd3ad5a9b81e153
Yosuke Kabuto <bluewhale1982@gmail.com>
2013-10-06 23:37:17 +0900

    initial commit

---
+++ opai.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <time.h>
+
+void main()
+{
+  struct stat theStat;
+  FILE *fp;
+
+  fp = fopen("opai.txt", "w");
+
+  if (fp)
+  {
+    int fsize = 0;
+    int mb = 1024 * 1024; // 1MB
+
+    for (fsize; fsize < mb; fsize = theStat.st_size)
+    {
+      stat("opai.txt", &theStat);
+      fprintf(fp, "おっぱい");
+    }
+
+    fclose(fp);
+  }
+}
